22bcbe8fabf02640dd9a7891996f1da5b4e3dd5f
sofiaelverdin <sofia.elverdin@yale.edu>
2021-07-07 19:38:09 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## As indicated in the assignment, the goal of the functions is caching the inverse of a matrix
 
-## Write a short comment describing this function
+## As requested in the assignment, this function seeks to create a  creates a matrix object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## As requested in the assignment, this function seeks to calculate and obtain the inverse of the matrix that we previously had
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
